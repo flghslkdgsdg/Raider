94c4ab2e0b3706eb9dc59037884d517da5fe805f
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-10-06 04:20:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,24 @@
- client.on('ready', function(){
-    var ms = 1000 ;
-    var setGame = [`$h `,` $he`,`$hel`,`$help`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
+  client.on("message", message => {
+    var prefix = "$";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "Mrzizx" // غير هنا حط اسم البوت
         }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
-    }, ms);1000
+      }}).then(msg => {msg.delete(3000)});
+                          }
 
+     
 });
   
 client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
