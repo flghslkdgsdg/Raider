283dbcfdc7dd6688311dd13923b87723177d1963
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-10-06 04:08:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-  let prefix = "بريفيكس بوتك";
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -12,46 +11,7 @@ client.on('message', msg => {
   }
 });
 
-  
-client.on("message", message => {
-  if(message.content.startsWith(prefix + "bc")) {
-    let filter = m => m.author.id === message.author.id;
-    let thisMessage;
-    let thisFalse;
-    message.channel.send(':regional_indicator_b::regional_indicator_c:| **ارسل الرسالة الان**').then(msg => {
-
-    let awaitM = message.channel.awaitMessages(filter, {
-      max: 1,
-      time: 20000,
-      errors: ['time']
-    })
-    .then(collected => {
-      collected.first().delete();
-      thisMessage = collected.first().content;
-      msg.edit(':regional_indicator_b::regional_indicator_c:| **هل انت متأكد؟**');
-      let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
-        max: 1,
-        time: 20000,
-        errors: ['time']
-      })
-      .then(collected => {
-        if(collected.first().content === 'لا') {
-          msg.delete();
-          message.delete();
-          thisFalse = false;
-        }
-        if(collected.first().content === 'نعم') {
-          if(thisFalse === false) return;
-        message.guild.members.forEach(member => {
-          msg.edit(':regional_indicator_b::regional_indicator_c:| **جاري الارسال**');
-          collected.first().delete();
-          member.send(`${thisMessage}\n\n${member} ,\nتم الارسال من : ${message.guild.name}\n تم الارسال بواسطة : ${message.author.tag}`);
-        });
-        }
-      });
-    });
-    });
-  }
-});
-
 client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
+
+
+
