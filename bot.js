9cc65e2c35643ce9c4f4f75283c34d2e6d13a726
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-10-06 04:13:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,18 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+  client.on('ready', function(){
+    var ms = 1000 ;
+    var setGame = [`Forèst `,` Fhelp`,` Forèst`,`Fhelp`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
+    }, ms);1000
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
-
-
-
+  
