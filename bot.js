003d158f0289e5fbc8c16161f0974a214e2b42f0
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-10-06 03:47:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
   client.on("message", message => {
-        var prefix = "+";
+        var prefix = "$";
     if(message.content.startsWith(prefix + "server")) {
-        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**ليس لديك البرمشن المطلوب لاستخدام هذا الامر ❌**");
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**ليس لديك البرمشن المطلوب لاستخدام هذا الامر **");
         const embed = new Discord.RichEmbed()
         .setAuthor(message.guild.name, message.guild.iconURL)
         .setColor("RANDOM")
 
-.addField('**عدد اعضاء السيرفر 👤 **' , `${message.guild.memberCount}`)
-.addField('**اونر السيرفر 👑**' , `${message.guild.owner.user.username}`)
+.addField('**عدد اعضاء السيرفر  **' , `${message.guild.memberCount}`)
+.addField('**اونر السيرفر **' , `${message.guild.owner.user.username}`)
 .addField(`**الرومات :scroll: **`,true)
 .addField(`# الكتابية`, `${message.guild.channels.filter(m => m.type === 'text').size}`)
 .addField( `:loud_sound: الصوتية`,`${message.guild.channels.filter(m => m.type === 'voice').size}`)
